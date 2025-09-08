a605ce56fe162ae8b80fefea7598b088d92a5794
mdarman85001 <md.arman85001@gmail.com>
2025-09-08 17:06:50 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+# Get these from https://my.telegram.org
+API_ID = int(os.environ.get("API_ID", "22467463"))
+API_HASH = os.environ.get("API_HASH", "156c0c4ecf6c03b4eb4ea9ac36e465d0")
+
+# Get from @BotFather
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8116851446:AAEONzMvHl1hy1m7knM8vfUaiolKXjaWLn8")
